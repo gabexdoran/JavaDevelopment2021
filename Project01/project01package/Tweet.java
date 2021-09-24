31673c3c52e7e90d3123a323a573cf122e0fb769
gabexdoran <gdoran1@cub.uca.edu>
2021-09-23 19:43:58 -0500

    19:43, 09/21/21

--- Project01/project01package/Tweet.java
+++ Project01/project01package/Tweet.java
@@ -1,5 +1,32 @@
 package project01package;
 
-public class Tweet {
+import java.util.ArrayList;
 
+public class Tweet {
+	private long id;
+	private String user;
+	private ArrayList<String> tweet;
+	
+	public Tweet(long id, String user) {
+		id = id;
+		user = user;
+		
+		tweet = new ArrayList<String>();
+	}
+	
+	public long getID(long id) {
+		return id;
+	}
+	
+	public void setID(long id) {
+		this.id = id;
+	}
+	
+	public String getUser(String user) {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
 }
--- Project01/project01package/Tweets.java
+++ Project01/project01package/Tweets.java
@@ -1,5 +1,23 @@
 package project01package;
 
-public class Tweets {
+import java.util.ArrayList;
 
+public class Tweets {
+	private ArrayList<Tweet> tweets;
+	
+	public boolean add(Tweet tweet) {
+		return true;
+	}
+	
+	public boolean addAll(ArrayList<Tweet> tweets) {
+		return true;
+	}
+	
+	public boolean remove(Tweet tweet) {
+		return true;
+	}
+	
+	public boolean removeAll(ArrayList<Tweet> tweets) {
+		return true;
+	}
 }
